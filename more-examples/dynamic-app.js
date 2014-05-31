f8583d01120eae26a91d9f27d25d813ecb0aa6e2
Ghislain B. <gbeaulac@gmail.com>
2014-05-31 01:25:57 -0400

    Fix #5 - Created example with dynamic input
    This comes from someone else using my validation directive, he created
    another directive (AE) which I added inside my folder "more-examples"
    folder.

---
+++ more-examples/dynamic-app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var myApp = angular.module('myApp', ['ngRoute', 'pascalprecht.translate', 'ghiscoding.validation']);
+
+myApp
+  // Routing
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/dynamic', {
+          templateUrl: 'dynamicForm.html',
+          controller: 'CtrlDynamic'
+    });
+    $routeProvider.otherwise({
+          redirectTo: 'dynamic',
+    });
+  })
+	.config(function ($translateProvider) {
+	  $translateProvider.useStaticFilesLoader({
+	    prefix: '../locales/validation/',
+	    suffix: '.json'
+		});
+  
+  	// load English ('en') table on startup
+		$translateProvider.preferredLanguage('en');
+	})
+  .controller('CtrlDynamic', ['$scope', '$translate', function ($scope, $translate) {
+    $scope.form1 = {};
+    
+    $scope.switchLanguage = function (key) {
+      $translate.uses(key);
+    };
+
+    $scope.data = [{
+        'name': "abc",
+        'title': 'first',
+        'validate':"required"
+    }, {
+        'name': "xyz",
+        'title': 'second',
+        'validate':"max_len:12|required"
+    }, {
+        'name': "std",
+        'title': 'third',
+        'validate':"max_len:22|required"
+    }];
+  }])
+  .directive('dyninp', function ( ) {
+    return {
+        restrict: 'AE',
+        scope: {
+            modal: '=',
+            validate:'='
+        },
+        replace: true,   
+        template: '<input type="text" class="form-control" ng-model="modal" validation="{{validate}}" />',
+        link: function (scope, element, attrs) {}
+    }
+});
